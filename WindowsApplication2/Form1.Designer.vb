c277b516a409db3c40bde77f5980873ea40b5caf
rsiu01 <rsiu01@gmail.com>
2017-01-19 11:06:02 -0500

    dev

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BrowseButton = New System.Windows.Forms.Button()
+        Me.cpfilepath = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OK_Button = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Source96RadioBtn = New System.Windows.Forms.RadioButton()
+        Me.Source384RadioBtn = New System.Windows.Forms.RadioButton()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox6 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox7 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox8 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.BrowseButton)
+        Me.GroupBox1.Controls.Add(Me.cpfilepath)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 166)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(550, 55)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Location of Cherry Pick File"
+        '
+        'BrowseButton
+        '
+        Me.BrowseButton.Location = New System.Drawing.Point(462, 19)
+        Me.BrowseButton.Name = "BrowseButton"
+        Me.BrowseButton.Size = New System.Drawing.Size(78, 20)
+        Me.BrowseButton.TabIndex = 2
+        Me.BrowseButton.Text = "Browse"
+        Me.BrowseButton.UseVisualStyleBackColor = True
+        '
+        'cpfilepath
+        '
+        Me.cpfilepath.Location = New System.Drawing.Point(6, 19)
+        Me.cpfilepath.Name = "cpfilepath"
+        Me.cpfilepath.Size = New System.Drawing.Size(450, 20)
+        Me.cpfilepath.TabIndex = 1
+        Me.cpfilepath.Text = "C:\"
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OK_Button.Location = New System.Drawing.Point(484, 290)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(78, 65)
+        Me.OK_Button.TabIndex = 1
+        Me.OK_Button.Text = "OK"
+        Me.OK_Button.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Source96RadioBtn)
+        Me.GroupBox2.Controls.Add(Me.Source384RadioBtn)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(269, 70)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Source Plate Format"
+        '
+        'Source96RadioBtn
+        '
+        Me.Source96RadioBtn.AutoSize = True
+        Me.Source96RadioBtn.Location = New System.Drawing.Point(137, 34)
+        Me.Source96RadioBtn.Name = "Source96RadioBtn"
+        Me.Source96RadioBtn.Size = New System.Drawing.Size(61, 17)
+        Me.Source96RadioBtn.TabIndex = 1
+        Me.Source96RadioBtn.Text = "96 Well"
+        Me.Source96RadioBtn.UseVisualStyleBackColor = True
+        '
+        'Source384RadioBtn
+        '
+        Me.Source384RadioBtn.AutoSize = True
+        Me.Source384RadioBtn.Checked = True
+        Me.Source384RadioBtn.Location = New System.Drawing.Point(41, 34)
+        Me.Source384RadioBtn.Name = "Source384RadioBtn"
+        Me.Source384RadioBtn.Size = New System.Drawing.Size(67, 17)
+        Me.Source384RadioBtn.TabIndex = 0
+        Me.Source384RadioBtn.TabStop = True
+        Me.Source384RadioBtn.Text = "384 Well"
+        Me.Source384RadioBtn.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Location = New System.Drawing.Point(294, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(269, 70)
+        Me.GroupBox3.TabIndex = 0
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Destination Plate Format"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 88)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(269, 65)
+        Me.GroupBox4.TabIndex = 3
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Liquid Class"
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Location = New System.Drawing.Point(293, 88)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(269, 65)
+        Me.GroupBox5.TabIndex = 4
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Tip Type"
+        '
+        'GroupBox6
+        '
+        Me.GroupBox6.Location = New System.Drawing.Point(12, 227)
+        Me.GroupBox6.Name = "GroupBox6"
+        Me.GroupBox6.Size = New System.Drawing.Size(144, 57)
+        Me.GroupBox6.TabIndex = 5
+        Me.GroupBox6.TabStop = False
+        Me.GroupBox6.Text = "Log Bar Codes?"
+        '
+        'GroupBox7
+        '
+        Me.GroupBox7.Location = New System.Drawing.Point(176, 227)
+        Me.GroupBox7.Name = "GroupBox7"
+        Me.GroupBox7.Size = New System.Drawing.Size(387, 57)
+        Me.GroupBox7.TabIndex = 6
+        Me.GroupBox7.TabStop = False
+        Me.GroupBox7.Text = "Bar Code Log File Name"
+        '
+        'GroupBox8
+        '
+        Me.GroupBox8.Location = New System.Drawing.Point(12, 290)
+        Me.GroupBox8.Name = "GroupBox8"
+        Me.GroupBox8.Size = New System.Drawing.Size(117, 65)
+        Me.GroupBox8.TabIndex = 7
+        Me.GroupBox8.TabStop = False
+        Me.GroupBox8.Text = "# Picks Per Wash"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(574, 378)
+        Me.Controls.Add(Me.GroupBox8)
+        Me.Controls.Add(Me.GroupBox7)
+        Me.Controls.Add(Me.GroupBox6)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.OK_Button)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Plate Format"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents BrowseButton As System.Windows.Forms.Button
+    Friend WithEvents cpfilepath As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox5 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox6 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox7 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox8 As System.Windows.Forms.GroupBox
+    Friend WithEvents Source96RadioBtn As System.Windows.Forms.RadioButton
+    Friend WithEvents Source384RadioBtn As System.Windows.Forms.RadioButton
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub BrowseButton_Click(sender As Object, e As EventArgs) Handles BrowseButton.Click
+
+        'set default directory to cherry pick folder
+        OpenFileDialog1.InitialDirectory = "C:\Users\Administrator\Documents\EVO Programs\384-Well CherryPicks"
+        'open csv files only
+        OpenFileDialog1.Filter = "csv files (*.csv)|*.csv"
+        OpenFileDialog1.ShowDialog()
+
+        'close form if openfiledialog was cancelled
+        If OpenFileDialog1.FileName = "" Then
+            Close()
+        End If
+
+        'saving location of cherry pick file
+        cpfilepath.Text = OpenFileDialog1.FileName
+
+    End Sub
+
+End Class
+
